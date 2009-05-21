a3a8245dba5e66c38fa85659d374fee2b8692c0a
anton.gogolev <anton.gogolev@localhost>
2009-05-21 14:38:20 +0000

    * Working on constraints

--- src/octalforty.Wizardby.Core/Db/DefaultDbNamingStrategy.cs
+++ src/octalforty.Wizardby.Core/Db/DefaultDbNamingStrategy.cs
@@ -21,7 +21,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 #endregion
-
 using octalforty.Wizardby.Core.SemanticModel;
 
 namespace octalforty.Wizardby.Core.Db
